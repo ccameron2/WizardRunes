5dcfe04e7ba5d2f0afdcb13b87c9e3f2776c0928
ccameron2 <74011941+ccameron2@users.noreply.github.com>
2022-12-13 22:33:03 +0000

    Initial commit

---
+++ WizardRunes/Source.cpp
@@ -0,0 +1,58 @@
+// Write a program that bubble sorts a random list of numbers without using alphanumerics
+
+#define _ int 
+#define ___ while
+
+// Can be done with just
+#define __ decltype
+
+// Decltype int. This resolves to the same as typing int
+//decltype(0)
+// Decltype void. This is the decltype of the call to a lambda function with no parameters or body
+//decltype([]() {}());
+
+#include <iostream>
+using namespace std;
+
+_ main() 
+{
+
+
+	///////////////////////////////////////////////////
+	_ $$ = {};
+	_ $ = {};
+	_ $$$$ = {};
+	_ $$$$$ = {};
+	$$$$++;
+
+	$++; $++; $++; $++; $++; $++; $++; $++; $++; $++;
+	
+	_ $$$[]{ 1,4,7,9,3,2,5,6,8,7 };
+
+	___($$$$ < $)
+	{
+		___ ($$$$$ < $)
+		{
+
+			___ ($$$[$$$$$] < $$$[$$$$]) 
+			{
+				$$ = $$$[$$$$];
+				$$$[$$$$] = $$$[$$$$$];
+				$$$[$$$$$] = $$;
+			}
+			$$$$$++;
+		}
+		$$$$++;
+		$$$$$ = {};
+	}
+
+	//////////////////////////////////////
+	for (_ $$$$$$ : $$$) 
+	{
+		cout << $$$$$$;
+	}
+
+	// Resolves to a number. Numbers can be produced using this method
+	char a = '-';
+	a -= a;
+}
